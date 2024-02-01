45f020c094c2b9f045e3d1e9985c477654bde4fd
Shivansh <shivansh18181818@gmail.com>
2024-02-01 16:11:46 +0530

    adding db

---
+++ React/Week5/todo-app-react/backend/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+dburl = ""
+mongoose.connect(url);
+
+
+const todoSchema = mongoose.Schema({
+    title:String,
+    description : String,
+    completed : Boolean
+})
+
+const todo = mongoose.model("todos",todoSchema);
+module.exports ={
+    todo
+}
